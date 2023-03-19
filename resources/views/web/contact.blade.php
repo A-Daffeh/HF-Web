64acaf30dba6db79cefe0c6d716cdb840d5cdb35
alieu22 <alieusma@gmail.com>
2023-03-18 22:26:14 -0700

    I added the html structure for the contact form

--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -1,18 +1,37 @@
 @extends('app')
 
 @section('content')
+ <!-- This is Alieu Samateh's section -->
 <h1>Interested in Contacting us?</h1>
-<h2>For any questions regarding our home care adult family home, pkease complete this form</h2>
+<h2>For any questions regarding our home care adult family home, please complete this form</h2>
 
-
-    <p>This page will be the contact page which will include the form and then a message telling the user thanks for applying</p>
+        <!-- This page will be the contact page which will include the form and then a message telling the user thanks for applying -->
+        
     <form>
-        <div></div>
-       <div>Name</div>
-       <div>Phone Number</div>
-       <div>Email</div>
-       <div>Reason for contact</div>
-       <div> Submit </div>
+       <div>
+        <label for="name"> Name </label>
+     <input type="text" id="name" placeholder="Enter your name" >
+       </div>
+
+     <div>
+     <label for="phoneNumber"> Phone Number </label>
+     <input type="text" id="phoneNumber" placeholder="Enter your number">
+     </div>
+      
+       <div>
+       <label for="emailAddress"> Email Address </label>
+       <input type="text" id="emailAddress" placeholder="Enter your email">
+       </div>
+
+       <div>
+       <label for="commentBox"> Reason for contact </label>
+       <input type="text" id="commentBox" placeholder="Write here">
+       </div>
+       
+       <div>
+        <input type="submit">
+     </div>
+
        
         
     </form>
