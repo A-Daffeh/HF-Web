f7810d4e353fae6cbd31bedfaac239a53907c612
alieu22 <alieusma@gmail.com>
2023-03-18 20:38:05 -0700

    I just added a paragraph explaing what the page will have

--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <div>
-    <h2>Contact PAGE</h2>
+    <h2>Contact PAGE</h2>  
+    <p>This page will be the contact page which will include the form and then a message telling the user thanks for applying</p>
 </div>
 @endsection
